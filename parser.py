bb633d764ed697f86941006b2e202b476b149a18
virtuNat <janlothan@gmail.com>
2017-11-07 14:44:24 +0800

    Prototype AST

--- parser.py
+++ parser.py
@@ -1,7 +1,153 @@
+import operator
+
 class AthExpr(object):
     __slots__ = ()
-    def __eq__(self, other):
-        pass
+
+    def __eq__(self, othr):
+        if isinstance(othr, self.__class__):
+            try:
+                self_attrs = self.__dict__
+                othr_attrs = othr.__dict__
+            except AttributeError:
+                self_attrs = {slot: getattr(self, slot) for slot in self.__slots__}
+                othr_attrs = {slot: getattr(othr, slot) for slot in othr.__slots__}
+            return self_attrs == othr_attrs
+        return False
 
     def __hash__(self):
-        pass
+        return object.__hash__(self)
+
+
+class ArithExpr(AthExpr):
+    """Superclass to all arithmetic-based syntax."""
+
+
+class NumExpr(ArithExpr):
+    """Superclass of both integers and floats."""
+    __slots__ = ('num')
+
+    def __init__(self, num):
+        self.num = num
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self.num)
+
+
+class IntExpr(NumExpr):
+    pass
+
+
+class FloatExpr(NumExpr):
+    pass
+
+
+class UnaryArithExpr(ArithExpr):
+    pass
+
+
+class BinaryArithExpr(ArithExpr):
+    pass
+
+
+class StringExpr(AthExpr):
+    pass
+
+
+class AthFunction(AthExpr):
+    """Superclass of all ~ATH functions."""
+
+
+class PrintFunc(AthFunction):
+    pass
+
+
+class KillFunc(AthFunction):
+    pass
+
+
+class AthSymbol(AthExpr):
+    """~ATH Variable data structure."""
+
+
+class BoolExpr(AthExpr):
+    """Superclass to all boolean syntax."""
+
+
+class ValueBoolExpr(BoolExpr):
+    """Superclass to all value-based boolean syntax."""
+
+
+class NotExpr(ValueBoolExpr):
+    pass
+
+
+class AndExpr(ValueBoolExpr):
+    pass
+
+
+class OrExpr(ValueBoolExpr):
+    pass
+
+
+class XorExpr(ValueBoolExpr):
+    pass
+
+
+class SymBoolExpr(BoolExpr):
+    """Superclass to all symbol-based boolean syntax."""
+
+
+class BothSymBool(SymBoolExpr):
+    pass
+
+
+class LeftSymBool(SymBoolExpr):
+    pass
+
+
+class RightSymBool(SymBoolExpr):
+    pass
+
+
+class NegLeftSymBool(SymBoolExpr):
+    pass
+
+
+class NegRightSymBool(SymBoolExpr):
+    pass
+
+
+class NegBothSymBool(SymBoolExpr):
+    pass
+
+
+class Statement(AthExpr):
+    """Superclass to all builtin statements."""
+
+
+class BirthStmt(Statement):
+    pass
+
+
+class BifurcateStmt(Statement):
+    pass
+
+
+class AggregateStmt(Statement):
+    pass
+
+
+class WhenStmt(Statement):
+    pass
+
+
+class UnlessStmt(Statement):
+    pass
+
+
+class InputStmt(Statement):
+    pass
+
+
+class TilDeathLoop(Statement):
+    pass
