03c1e5822f65ea151a4cda2bb84fd0497ae6fb3c
virtuNat <janlothan@gmail.com>
2017-11-07 13:46:49 +0800

    Cull grafter

--- grafter.py
+++ grafter.py
@@ -76,26 +76,6 @@ class Evaluator(Grafter):
         return graft
 
 
-class Exp(Parser):
-    def __init__(self, parser, separator):
-        self.parser = parser
-        self.separator = separator
-
-    def __call__(self, tokens, pos):
-        result = self.parser(tokens, pos)
-
-        def process_next(parsed):
-            (sepfunc, right) = parsed
-            return sepfunc(result.value, right)
-        next_parser = self.separator + self.parser ^ process_next
-
-        next_result = result
-        while next_result:
-            next_result = next_parser(tokens, result.pos)
-            if next_result:
-                result = next_result
-        return result
-
 class ExprParser(Grafter):
     """A grafter that evaluates expressions using two grafters.
 
