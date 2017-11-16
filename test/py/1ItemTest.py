c743744d6d323346ce1348008805802f201ce0b1
theSardonyx <kryzldeannep@gmail.com>
2017-11-16 22:25:59 +0800

    Interpreter Beta Test #5
    [PASSED]
    - InfiniteLoopTest
    - GetRoots2
    - CountTo100
    - AddOneFunc
    
    [FAILED]
    - 1ItemTest (wrong output)
    - 1ItemTest2 (wrong output)
    - AggregateBifurcateTest
    - GetRoots
    - ScopeTest (wrong output)
    *Traceback can be found in the respective .out files

---
+++ test/py/1ItemTest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from athast import *
+from athparser import TildeAthInterp
+
+ath_script = Serialize([ProcreateStmt(VarExpr('LOOP'), IntExpr(0)), TildeAthLoop(VarExpr('LOOP'), Serialize([PrintFunc([StringExpr('Which of the following is not a French word?\\n')]), PrintFunc([StringExpr('[A] sale\\n')]), PrintFunc([StringExpr('[B] mode\\n')]), PrintFunc([StringExpr('[C] grand\\n')]), PrintFunc([StringExpr('[D] chat\\n')]), PrintFunc([StringExpr('[E] A, B, C, and D are all French words\\n')]), PrintFunc([StringExpr('[F] A, B, C, and D are all not French words\\n')]), InputStmt(VarExpr('CHOICE'), StringExpr('')), DebateStmt(BinaryExpr('<=', VarExpr('CHOICE'), StringExpr('F')), Serialize([PrintFunc([StringExpr('Wrong! Try again, idiot.')])], LOOP), [UnlessStmt(BinaryExpr('==', VarExpr('CHOICE'), StringExpr('THE CHEAT CODE')), Serialize([PrintFunc([StringExpr("Damn, you caught me. It's a trick question.")])], LOOP)), UnlessStmt(BinaryExpr('==', VarExpr('CHOICE'), StringExpr('DIE')), Serialize([PrintFunc([StringExpr('Hmph. Ninny.')]), KillFunc(VarExpr('LOOP'), [])], LOOP)), UnlessStmt(None, Serialize([PrintFunc([StringExpr("HA! Loser can't even guess the cheat code.")])], LOOP))])], LOOP)), KillFunc(VarExpr('THIS'), [])], THIS)
+TildeAthInterp().execute(ath_script)
